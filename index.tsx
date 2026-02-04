024698e8f2ba563bc8fdff72354e3955abf9f5af
Maxim Fedorov <maximfedorov@Noutbuk-Maxim.local>
2026-02-04 16:37:52 +0300

    feat: add Sentry SDK to frontend

--- index.tsx
+++ index.tsx
@@ -4,6 +4,23 @@ import App from './App';
 import { AuthProvider } from './contexts/AuthContext';
 import { SettingsProvider } from './contexts/SettingsContext';
 
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://eb23b5bd7d87cc4c6d724953dcce10f9@o4510827913412608.ingest.de.sentry.io/4510827937202256",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, always sample the session when an error occurs.
+});
+
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
